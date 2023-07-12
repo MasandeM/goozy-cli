6420e5c6aa698d30cf723ff96ca173492d58dbf6
Masande Mtintsilana <masande@cachefault.com>
2023-07-12 12:15:21 +0100

    add support for controlling light colors

--- goozy/goozy.py
+++ goozy/goozy.py
@@ -1,7 +1,14 @@
 import asyncio
 import click
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 from functools import wraps
+
+LIGHTS_CHARACTERISTIC = "0000ffe1-0000-1000-8000-00805f9b34fb"
+COLORS = {
+    "red" : "FF0000",
+    "green" : "00FF00",
+    "blue" : "0000FF"
+}
 
 def coro(f):
     @wraps(f)
@@ -34,6 +41,27 @@ async def discover():
     else:
         click.echo("[-] No light bars discovered. Ensure light bar is on and is not paired with any BLE clients")
 
+@cli.command()
+@click.option('--address', required=True)
+@click.option('--on/--off', default=False)
+@click.option('--color')
+@coro
+async def control(address, on, color):
     
+    client = BleakClient(address)
+    try:
+        await client.connect()
+        if on:
+            await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex("A06904FFFFFFFF"))
+
+        elif color:
+            if color in COLORS.keys():                
+                hexcolor = "A06904{}FF".format(COLORS[color])
+                await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex(hexcolor))
+
+        await asyncio.sleep(2)
+    finally:
+        await client.disconnect()
+
 if __name__ == '__main__':
     cli()
