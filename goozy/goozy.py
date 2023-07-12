e6f261e965e0b1095b2299ff80ac431851141ec8
Masande Mtintsilana <masande@cachefault.com>
2023-07-12 12:53:09 +0100

    more colors

--- goozy/goozy.py
+++ goozy/goozy.py
@@ -5,9 +5,22 @@ from functools import wraps
 
 LIGHTS_CHARACTERISTIC = "0000ffe1-0000-1000-8000-00805f9b34fb"
 COLORS = {
-    "red" : "FF0000",
-    "green" : "00FF00",
-    "blue" : "0000FF"
+    'white':'FFFFFF',
+    'silver':'C0C0C0',
+    'gray':'808080',
+    'black':'000000',
+    'red':'FF0000',
+    'maroon':'800000',
+    'yellow':'FFFF00',
+    'olive':'808000',
+    'lime':'00FF00',
+    'green':'008000',
+    'aqua':'00FFFF',
+    'teal':'008080',
+    'blue':'0000FF',
+    'navy':'000080',
+    'fuchsia':'FF00FF',
+    'purple':'800080'
 }
 
 def coro(f):
