d62c92552d978519733819e84d7709bbfd71ff7d
Masande Mtintsilana <masande@cachefault.com>
2023-07-16 22:55:02 +0100

    add support for adjusting brightness

--- goozy/goozy.py
+++ goozy/goozy.py
@@ -1,5 +1,8 @@
 import asyncio
 import click
+import os
+from configparser import ConfigParser
+import colorsys
 from bleak import BleakScanner, BleakClient
 from functools import wraps
 
@@ -59,24 +62,57 @@ async def discover():
 @click.option('--on', default=False, is_flag=True, flag_value=True)
 @click.option('--off', default=False, is_flag=True, flag_value=True)
 @click.option('--color')
+@click.option('--brightness')
 @coro
-async def control(address, on, off, color):
+async def control(address, on, off, color, brightness):
     
     client = BleakClient(address)
     try:
         await client.connect()
+        config_file_path = 'config.ini'
+        config = ConfigParser()
+
+        if os.path.isfile(config_file_path):
+            config.read(config_file_path)
+        else:
+            config.add_section(address)
+
         if on:
             await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex("A06904FFFFFFFF"))
+
         elif off:
             await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex("A06904000000FF"))
-        elif color:
+
+        if color:
             if color in COLORS.keys():                
-                hexcolor = "A06904{}FF".format(COLORS[color])
-                await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex(hexcolor))
+                hexcolor = COLORS[color]
+                ble_command = "A06904{}FF".format(hexcolor)
+
+                await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex(ble_command))
+                config.set(address, 'hexcolor',  hexcolor)
+
+        if brightness:
+            hexcolor = config.get(address, 'hexcolor')
+            value = str(adjust_brightness(hexcolor, int(brightness)/100))
+            ble_command = "A06904{}FF".format(value)
+            await client.write_gatt_char(LIGHTS_CHARACTERISTIC, bytes.fromhex(ble_command))
 
         await asyncio.sleep(2)
     finally:
         await client.disconnect()
+        with open(config_file_path, 'w') as f:
+            print(config)
+            config.write(f)
+
+def adjust_brightness(hex_color, brightness):
+
+    rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    hsb = colorsys.rgb_to_hsv(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
+
+    adjusted_hsb = tuple([hsb[0], hsb[1], hsb[2] * brightness])
+    adjusted_rgb = colorsys.hsv_to_rgb(adjusted_hsb[0], adjusted_hsb[1], adjusted_hsb[2])
+
+    return '{:02x}{:02x}{:02x}'.format(int(adjusted_rgb[0]*255), int(adjusted_rgb[1]*255), int(adjusted_rgb[2]*255))
 
 if __name__ == '__main__':
     cli()
